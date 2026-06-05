df3419c9e8bb8e45558f1d587618857397996cb7
ritesh-1918 <bonthalamadhavi1@gmail.com>
2026-06-05 15:43:23 +0530

    security: remove default postgres password from test suite to resolve GitGuardian alert

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -17,7 +17,7 @@ def db_connection():
             port=os.getenv("DB_PORT", "5432"),
             dbname=os.getenv("DB_NAME", "postgres"),
             user=os.getenv("DB_USER", "postgres"),
-            password=os.getenv("DB_PASSWORD", "postgres"),
+            password=os.getenv("DB_PASSWORD", ""),
         )
 
         yield connection
